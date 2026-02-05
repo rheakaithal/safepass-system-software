65e8fce800619245c3d767a145fd5f19d0062551
Parker Williamson <parker.d.williamson@gmail.com>
2026-02-05 17:38:31 -0600

    Updated demo code

--- backend/MQTT_testCode/demoPublish.py
+++ backend/MQTT_testCode/demoPublish.py
@@ -14,12 +14,13 @@ direction2 = 1
 
 while 1:
     print(pole1Value, pole2Value)
-    msg = [{'topic':"sensor/1/waterlevel", 'payload':str(round(pole1Value,2))},{'topic':"sensor/2/waterlevel", 'payload':str(round(pole2Value,2))}]
+    msg = [{'topic':"sensor/1/waterlevel", 'payload':str(round(pole1Value,2))},
+           {'topic':"sensor/2/waterlevel", 'payload':str(round(pole2Value,2))}]
     publish.multiple(
         msg,
-        hostname="83ad0f202f85425e99ee81ecdda5e543.s1.eu.hivemq.cloud",             # Broker IP address
+        hostname="83ad0f202f85425e99ee81ecdda5e543.s1.eu.hivemq.cloud",
         port=8883,
-        auth={"username":"Sensor", "password":"Team13Capstone"}, # Authentication
+        auth={"username":"Sensor", "password":"Team13Capstone"},
         tls={}
     )
     if(pole1Value >= 8.0 or pole1Value <= 0):
