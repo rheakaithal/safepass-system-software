70d1d4dbc1d932a94fa17f087ea26243b29c4f6c
Parker Williamson <parker.d.williamson@gmail.com>
2026-02-04 19:11:35 -0600

    Working on layout

--- GUIv2/mqtt.js
+++ GUIv2/mqtt.js
@@ -1,19 +1,19 @@
 const mqtt = require('mqtt')
 const fs = require('fs')
 
-const host = '10.244.80.97'
-const port = '1883'
+const host = '83ad0f202f85425e99ee81ecdda5e543.s1.eu.hivemq.cloud'
+const port = '8883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
-const connectUrl = `mqtt://${host}:${port}`
+const connectUrl = `mqtts://${host}:${port}`
 
 const client = mqtt.connect(connectUrl, {
   clientId,
   clean: true,
   connectTimeout: 4000,
-  username: 'GUI',
+  username: 'Dashboard',
   password: 'Team13Capstone',
-  reconnectPeriod: 1000,
+  reconnectPeriod: 1000
 })
 
 const topic = 'sensors/2/waterlevel'
